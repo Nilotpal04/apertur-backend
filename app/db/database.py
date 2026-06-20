7f172c84c09401e015f8b2836a88c69e03284399
Nilotpal04 <nilotpalb2004@gmail.com>
2026-06-20 12:55:15 +0000

    setup MongoDB Atlas and Beanie

--- app/db/database.py
+++ app/db/database.py
@@ -1,11 +1,22 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-
+from beanie import init_beanie
+import certifi
 from app.core.config import settings
+from app.models.user import User
+import ssl
 
-
-client = AsyncIOMotorClient(settings.mongodb_url)
+client = AsyncIOMotorClient(
+    settings.mongodb_url,
+    tls=True,
+    tlsCAFile=certifi.where(),
+    tlsAllowInvalidCertificates=False,
+)
 
 
 async def connect_to_mongodb():
-    await client.admin.command("ping")
+    await init_beanie(
+        database=client.apertur_db,
+        document_models=[User]
+    )
+
     print("Connected to MongoDB Atlas")
---
+++ app/models/user.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from beanie import Document
+from pydantic import Field
+
+class User(Document):
+    username: str
+    email: str
+    hashed_password: str
+    
+    full_name: str | None = None
+    bio: str | None = None
+    avatar_url: str | None = None
+    
+    is_active: bool = True
+    
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    updated_at: datetime = Field(default_factory=datetime.utcnow)
+    
+    class  Settings:
+        name = "users"
